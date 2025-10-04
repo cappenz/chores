29f971354bdc64603e61f252e9cf89a64e940c88
cappenz <143547735+cappenz@users.noreply.github.com>
2025-10-03 21:12:09 -0700

    Update chores.py

--- chores.py
+++ chores.py
@@ -216,7 +216,7 @@ class ChoreStatus:
         minute = now.minute
         second = now.second
         # japanese_date = f"{year}年{month}月{day}日 {hour}時{minute}分{second}秒"
-        holloween_date = f"🎃 {day}/{Month}/{year} 🎃 {hour}:{minute}:{second} 🎃"
+        holloween_date = f"🎃 {day}/{month}/{year} 🎃 {hour}:{minute}:{second} 🎃"
         
         self.time_label.config(text=holloween_date)
         
